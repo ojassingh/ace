8df890c2181cc680c9ff3ba9dfb212c2a33c0537
Ojas Singh <ojas.singh02@gmail.com>
2022-09-20 13:22:52 -0400

    setup getStaticprops to fetch event docs from Firebase

--- components/Navi.jsx
+++ components/Navi.jsx
@@ -21,7 +21,7 @@ const Navi = () => {
                 // User is signed in, see docs for a list of available properties
                 // https://firebase.google.com/docs/reference/js/firebase.User
                 const uid = user.uid;
-                console.log('user is signed in: ' + uid);
+                // console.log('user is signed in: ' + uid);
                 setSession(true);
                 setButton(<Link href='/account' className="nav-link">[Account]</Link>)
                    
--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,34 +1,58 @@
 import Navi from "../../components/Navi";
 import styles from '../../styles/Events.module.scss';
-import { collection, doc, setDoc, getDocs, getDoc, DocumentReference} from "firebase/firestore";
+import { collection, doc, setDoc, getDocs, getDoc, DocumentReference, query, QuerySnapshot} from "firebase/firestore";
 import { database } from "../../firebase/config";
-import { Firestore } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
-const events = () => {
+const events = ({events}) => {
+
+    const eventList = JSON.parse(events);
+
+    useEffect(()=>{
+       eventList.map((event)=>{
+            console.log(event);
+       })
+    }, [])
+
+    // useEffect(()=>{
+    //     setData(eventList);
+    //     console.log(data);
+    // }, [])
 
 
     return(<div className={styles.container}>
         <Navi/>
         <h1>Events</h1>
         <ul>
-            <li></li>
+
+            {
+                eventList.map((event)=>{
+                    return(<li key={event.id}>
+                        {event.name}
+                    </li>)
+                })
+            }
+
+            
         </ul>
     </div>);
 }
 
+
+export const getStaticProps = async () => {
+    const entries = await getDocs((collection(database, "events")));
+    const data = entries.docs.map(entry => ({
+      id: entry.id,
+      ...entry.data()
+    }));
+
+    const events = JSON.stringify(data);
+
+    return {
+      props: { events },
+      revalidate: 10
+    }
+  }
+  
+
 export default events;
-
-// export async function getStaticPaths() {
-
-//     const querySnapshot = await getDocs(collection(database, "events"))
-    
-
-//     return {
-//       fallback: false,
-//       paths: [
-//         {params:{
-//             meetupI: "ev"
-//         }}
-//       ],
-//     }
-//   }
