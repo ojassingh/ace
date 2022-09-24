84b9c44e0b6a2ba169001a2179fa12f4c49ef313
Ojas Singh <ojas.singh02@gmail.com>
2022-09-24 18:22:58 -0400

    bug fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a6cab01f8ea6dcdc"; }
+/******/ 		__webpack_require__.h = function() { return "46a21b4f17b8d534"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
